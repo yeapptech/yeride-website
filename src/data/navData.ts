90b332e88e6036e17c7c0cc6c84c42539a6bc3e0
Hernando Sierra <hernando.sierra@yeapp.tech>
2024-09-07 12:28:40 -0400

    first commit

---
+++ src/data/navData.ts
@@ -0,0 +1,22 @@
+export const navData = [
+    {
+        name: "Home",
+        href: '/'
+    },
+    {
+        name: "Ride",
+        href: 'ride'
+    },
+    {
+        name: "Drive",
+        href: 'drive'
+    },
+    {
+        name: "About",
+        href: 'about'
+    },
+    {
+        name: "Register",
+        href: 'register'
+    },
+]
