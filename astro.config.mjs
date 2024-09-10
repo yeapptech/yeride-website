687262945d9e41ac19c906dc3f1642fe9dd533da
Hernando Sierra <hernando.sierra@yeapp.tech>
2024-09-10 19:54:02 -0400

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   // site: 'http://yeapptech.github.io',
   site: "https://yeride.com",
-  output: "server",
+  output: "server"
   // base: 'yeride-website',
+  ,
+  adapter: vercel()
 });
