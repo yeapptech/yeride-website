c47b1b44570444d20048e2c36957a766c5bc835d
Hernando Sierra <hernando.sierra@yeapp.tech>
2024-09-10 20:29:01 -0400

    debug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
-import vercel from "@astrojs/vercel/serverless";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
