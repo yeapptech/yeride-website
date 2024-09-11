2e41daf6aca9ac3d21d679643850dfe05e17b8c3
Hernando Sierra <hernando.sierra@yeapp.tech>
2024-09-10 22:14:05 -0400

    Reverse back to not sending beta registration to google spreedsheet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   // site: 'http://yeapptech.github.io',
   site: "https://yeride.com",
-  output: "server",
-  adapter: node({
-    mode: 'standalone',
-  }),
+  // output: "server",
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
 });
