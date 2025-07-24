7a18116c2c99bd4b0fbdef93d84b227df814e6d3
Yoselyn Castillo <yose.castillo06@gmail.com>
2025-07-24 01:26:10 -0400

    update insurance status

--- functions/src/routers/users/users.routes.ts
+++ functions/src/routers/users/users.routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { getUsers, getUsersById, getUsersByRole } from "../../controllers/users/getUsers.controller";
+import { updateDriverInsuranceStatus } from "../../controllers/users/updateUser.controller";
 
 const router = express.Router();
 
@@ -8,5 +9,8 @@ router.get("/", getUsers);
 router.get("/:id", getUsersByRole);
 
 router.get("/id/:id", getUsersById);
+
+router.patch("/:id/insurance-status", updateDriverInsuranceStatus);
+
 
 export default router;
