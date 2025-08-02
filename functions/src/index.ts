a5fee54a2719281497f19139b508218b40d87522
DrNFox <biltd.88@gmail.com>
2025-08-02 02:35:23 -0400

    CORS test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
-import { onRequest } from "firebase-functions/v2/https"; 
+import { onRequest } from "firebase-functions/v2/https";
 import router from "./routers";
 import cors from "cors";
+
 import { handleStripeRedirect } from "./controllers/redirect.controller.js";
 
 const app = express();
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-custom-header'],
+  credentials: true,
+}));
+
+app.use(express.json());
 
 app.use("/v1", router);
 
